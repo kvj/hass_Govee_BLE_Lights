225bb42870a75d3cfca5860ae8dcb7efac365bf0
Konstantin Vorobyev <konstantin.vorobyev@zalando.de>
2023-06-09 17:58:01 +0200

    Add BT discovery

--- custom_components/govee_ble_lights/config_flow.py
+++ custom_components/govee_ble_lights/config_flow.py
@@ -7,21 +7,39 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema({
-    vol.Required("name"): cv.string,
-    vol.Required("address"): cv.string,
-    vol.Required("model"): cv.string,
-})
-
-
 class ConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
-    async def async_step_user(self, user_input):
+    async def async_step_bluetooth(self, info):
+        _LOGGER.debug(f"BT Discovery: {info.as_dict()}")
+        await self.async_set_unique_id(info.address)
+        self._abort_if_unique_id_configured()
+        self.info = {
+            "address": info.address,
+            "model": info.name.split("_")[1],
+            "name": info.name,
+        }
+        return await self.async_step_input()
+    
+    async def async_step_user(self, user_input=None):
+        self.info = {
+            "address": "",
+            "model": "",
+            "name": "",
+        }
+        return await self.async_step_input()
+
+    async def async_step_input(self, user_input=None):
+        _LOGGER.debug(f"Input step: {user_input}")
         if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
-        _LOGGER.debug(f"Input: {user_input}")
+            schema = vol.Schema({
+                vol.Required("name"): cv.string,
+                vol.Required("address", default=self.info["address"]): cv.string,
+                vol.Required("model", default=self.info["model"]): cv.string,
+            })
+            return self.async_show_form(step_id="input", data_schema=schema)
+
         await self.async_set_unique_id(user_input["address"])
         self._abort_if_unique_id_configured()
-        return self.async_create_entry(title=user_input["name"], data=user_input)
+        return self.async_create_entry(title=user_input["name"], data=user_input, description_placeholders={
+            "name": self.info["name"],
+        })
